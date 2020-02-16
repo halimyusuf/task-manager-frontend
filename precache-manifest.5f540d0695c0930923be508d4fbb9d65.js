c6ace430f9d9cafe4f35e1e3b1e0bf4616a679ba
halim <haleemyoosuph@gmail.com>
2020-02-16 16:55:27 +0100

    Updates

---
+++ precache-manifest.5f540d0695c0930923be508d4fbb9d65.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3ce9a75b59d3d2775984c882d5b3ed",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "221705dc933e674f486f",
+    "url": "/task-manager/static/css/2.7a29d18c.chunk.css"
+  },
+  {
+    "revision": "de673c61c4dd36af7f26",
+    "url": "/task-manager/static/css/main.ad1d4fb2.chunk.css"
+  },
+  {
+    "revision": "221705dc933e674f486f",
+    "url": "/task-manager/static/js/2.2ad70a55.chunk.js"
+  },
+  {
+    "revision": "4ee549e1da2e412adef52a60b9376544",
+    "url": "/task-manager/static/js/2.2ad70a55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de673c61c4dd36af7f26",
+    "url": "/task-manager/static/js/main.cf5a4c56.chunk.js"
+  },
+  {
+    "revision": "e197435cfe805eee7fe0",
+    "url": "/task-manager/static/js/runtime-main.1eb68522.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/task-manager/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/task-manager/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/task-manager/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/task-manager/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/task-manager/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
