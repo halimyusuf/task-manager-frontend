071969c9300711502d9f43c2a9e0aa4c375efc94
halim <haleemyoosuph@gmail.com>
2020-02-17 00:25:05 +0100

    react-create-app on Heroku

--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,13 @@ import "./index.css";
 import App from "./App";
 import "font-awesome/css/font-awesome.css";
 import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />{" "}
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
